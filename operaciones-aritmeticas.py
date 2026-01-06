c4a914d8f802ec2729a6b398f3ea663d9992109d
Diego <71624462@continental.edu.pe>
2026-01-06 18:38:48 -0500

    Se imploemento una suma

--- operaciones-aritmeticas.py
+++ operaciones-aritmeticas.py
@@ -1,1 +1,8 @@
-print ("Hello World")   
+numero1 = int(input("Ingrese el primer numero: "))
+numero2 = int(input("Ingrese el primer numero: "))
+suma = numero1 + numero2
+print("La suma es:". suma)
+
+
+
+
