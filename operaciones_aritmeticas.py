b388d234f426b70ff5101c33529b23ddde752d8d
DCarhuamaca <140361129+Keterod@users.noreply.github.com>
2026-01-06 19:40:44 -0500

    Se implentó las cuatro operaciones aritméticas básicas

--- operaciones_aritmeticas.py
+++ operaciones_aritmeticas.py
@@ -1,20 +1,21 @@
-
 class operacionesAritmeticas:
-    def __int__(self, numero1=0, numero2=0):
-        self.numero1= numero1
-        self.nuemro2= numero2
+    def __init__(self, numero1=0, numero2=0):
+        self.numero1 = numero1
+        self.numero2 = numero2
 
     def sumar_dos_numeros(self):
-        return self.numero1 +self.numero2
-    
-if __name__ == "__main__":
-    numero1 = int(input("Ingrese el primer numero: "))
-    numero2 = int(input("Ingrese el segundo numero: "))
+        return self.numero1 + self.numero2
 
-operaciones= operacionesAritmeticas(numero1, numero2)
+    def restar_dos_numeros(self):
+        return self.numero1 - self.numero2
 
-print("La suma es:", operaciones.sumar_dos_numeros())
+    def multiplicar_dos_numeros(self):
+        return self.numero1 * self.numero2
 
+    def dividir_dos_numeros(self):
+        if self.numero2 == 0:
+            return "Error: División por cero"
+        return self.numero1 / self.numero2
 
 
 
